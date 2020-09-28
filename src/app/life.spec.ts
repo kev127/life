528a82943ca4871bd8771268182f7f6fe32b1fa3
kev127 <kelvinkeya125@gmail.com>
2020-09-28 11:04:50 +0300

    add blueprint class

---
+++ src/app/life.spec.ts
@@ -0,0 +1,7 @@
+import { Life } from './life';
+
+describe('Life', () => {
+  it('should create an instance', () => {
+    expect(new Life()).toBeTruthy();
+  });
+});
---
+++ src/app/life.ts
@@ -0,0 +1,4 @@
+export class Life {
+    constructor(public text: string,public author: string,public completeDate: Date ,public poster:string){
+}
+}
