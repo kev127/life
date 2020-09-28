af4838a355df4dbcb8c8a212fb60a8f6d899d96d
kev127 <kelvinkeya125@gmail.com>
2020-09-28 14:20:36 +0300

    add function on life component.ts

--- src/app/life/life.component.ts
+++ src/app/life/life.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Life } from '../life';
 
 @Component({
   selector: 'app-life',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./life.component.css']
 })
 export class LifeComponent implements OnInit {
+  life: Life[] = [
+    new Life('The Way Get Started Is To Quit Talking And Begin Doing.', '~Walt Disney',new Date(2020,3,14),'Jane'),
+    new Life('If life were predictable it would cease to be life, and be without flavor.','-Eleanor Roosevelt',new Date(2020,6,9), 'Peter'),
+    new Life('Life is what happens when you are busy making other plans.', '-John Lennon',new Date(2020,8,6), 'John'),
+  ];
 
   constructor() { }
 
