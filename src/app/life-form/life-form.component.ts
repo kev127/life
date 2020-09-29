23194275cf6cf12411027d626fa19224ba47394c
kev127 <kelvinkeya125@gmail.com>
2020-09-29 08:51:24 +0300

    add form component function to component.ts

--- src/app/life-form/life-form.component.ts
+++ src/app/life-form/life-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core'
+import { Life } from '../life';
 
 @Component({
   selector: 'app-life-form',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./life-form.component.css']
 })
 export class LifeFormComponent implements OnInit {
+  @Input() 
+  life: Life
+  newLife = new Life("", "", new Date(), "");
+  @Output() 
+  addLife = new EventEmitter<Life>();
+
+  submitlife() {
+    this.addLife.emit(this.newLife);
+  }
 
   constructor() { }
 
