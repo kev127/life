7a7206f7d0ff299860d4afca16246832351c88c0
kev127 <kelvinkeya125@gmail.com>
2020-09-29 08:57:23 +0300

    add new life component to life component.ts

--- src/app/life/life.component.ts
+++ src/app/life/life.component.ts
@@ -12,6 +12,12 @@ export class LifeComponent implements OnInit {
     new Life('If life were predictable it would cease to be life, and be without flavor.','-Eleanor Roosevelt',new Date(2020,6,9), 'Peter'),
     new Life('Life is what happens when you are busy making other plans.', '-John Lennon',new Date(2020,8,6), 'John'),
   ];
+  addNewLife(life){
+    let lifeLength = this.life.length;
+    life.id = lifeLength+1;
+    life.iscompleteDate = new Date(life.completeDate)
+    this.life.push(life)
+  }
 
   constructor() { }
 
